ea1478dc0519568c8f81f2cb67ff96dc04a6533a
carissab96 <bayscarissa706@gmail.com>
2025-04-03 22:28:52 -0500

    updated seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,10 +19,10 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_photos()
-        undo_groups()
         undo_events()
+        undo_groups()
+        undo_photos()
+        undo_users()
     seed_users()
     seed_photos()
     seed_groups()
