7b1ff1d5b651305ea751a9e30d5fa9494db1b679
carissab96 <bayscarissa706@gmail.com>
2025-04-03 23:38:05 -0500

    Fix Favorite model by removing duplicate tablename assignment in production mode

--- app/models/favorite.py
+++ app/models/favorite.py
@@ -9,8 +9,6 @@ class Favorite(db.Model):
     __tablename__ = 'favorites'
 
     if environment == "production":
-
-        __tablename__ = add_prefix_for_prod(__tablename__)
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
